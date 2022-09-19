c568a394ff05e8113d54de76aeb71f9b85575991
Martin Šmídl <smidl.martin1@gmail.com>
2022-09-19 15:51:36 +0200

    add register controller

--- src/controllers/register.ts
+++ src/controllers/register.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express'
+import { User, UserModel } from '../schemas/user'
 
 export const register = async (req: Request, res: Response) => {
   try {
+    const { name, password, confirmPassword } = req.body
+    console.log(name, password, confirmPassword)
+    res.send('register router')
   } catch (error) {}
 }
